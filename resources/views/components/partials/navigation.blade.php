c35ee68369b6b20800acaa1ea60a009cdc504b36
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2025-09-27 15:20:59 -0600

    Actualizar el componente de navegación para mejorar el diseño y la accesibilidad

--- resources/views/components/partials/navigation.blade.php
+++ resources/views/components/partials/navigation.blade.php
@@ -1,13 +1,14 @@
-<header class="bg-white dark:bg-gray-900">
+<header class="bg-white dark:bg-gray-900 fixed w-full z-30 top-0 lg:top-6">
   <nav aria-label="Global" class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
     <div class="flex lg:flex-1">
-      <a href="{{ route('home') }}" class="-m-1.5 p-1.5">
+      <a href="{{ route('home') }}" class="-m-1.5 p-1.5 flex items-center gap-x-3">
         <span class="sr-only">Achronyme - Engineering toolbox</span>
         <x-image.logo class="h-8 w-auto"
         src="resources/images/logo.png"
         alt="Achronyme - Engineering toolbox"
         priority=true
         />
+        <p class="text-md font-bold text-gray-900 dark:text-white">Achronyme</p>
       </a>
     </div>
     <div class="flex lg:hidden">
@@ -73,25 +74,11 @@
               </div>
             </div>
           </div>
-          <div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50 dark:divide-white/10 dark:bg-gray-700/50">
-            <a href="#" class="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700/50">
-              <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5 flex-none text-gray-400 dark:text-gray-500">
-                <path d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm6.39-2.908a.75.75 0 0 1 .766.027l3.5 2.25a.75.75 0 0 1 0 1.262l-3.5 2.25A.75.75 0 0 1 8 12.25v-4.5a.75.75 0 0 1 .39-.658Z" clip-rule="evenodd" fill-rule="evenodd" />
-              </svg>
-              Watch demo
-            </a>
-            <a href="#" class="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700/50">
-              <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5 flex-none text-gray-400 dark:text-gray-500">
-                <path d="M2 3.5A1.5 1.5 0 0 1 3.5 2h1.148a1.5 1.5 0 0 1 1.465 1.175l.716 3.223a1.5 1.5 0 0 1-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 0 0 6.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 0 1 1.767-1.052l3.223.716A1.5 1.5 0 0 1 18 15.352V16.5a1.5 1.5 0 0 1-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 0 1 2.43 8.326 13.019 13.019 0 0 1 2 5V3.5Z" clip-rule="evenodd" fill-rule="evenodd" />
-              </svg>
-              Contact sales
-            </a>
-          </div>
         </el-popover>
       </div>
 
-      <a href="#" class="text-sm/6 font-semibold text-gray-900 dark:text-white">Features</a>
-      <a href="#" class="text-sm/6 font-semibold text-gray-900 dark:text-white">Marketplace</a>
+      <a href="#" class="text-sm/6 font-semibold text-gray-900 dark:text-white">Fourier</a>
+      <a href="#" class="text-sm/6 font-semibold text-gray-900 dark:text-white">Convolución</a>
 
       <div class="relative">
         <button popovertarget="desktop-menu-company" class="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900 dark:text-white">
