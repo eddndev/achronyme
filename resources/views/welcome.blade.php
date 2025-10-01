f9dda5a3b2fd7a9c19d16b237bc24e796dc1918e
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2025-10-01 15:27:51 -0600

    feat(navigation): actualizar etiquetas y descripciones para Transformada de Fourier y Convolución

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         </a>
     </div>
     <div class="mx-auto mt-8 max-w-2xl text-center">
-        <h2 class="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl dark:text-white"><span class="animated-gradient text-7xl">Achronyme</span><br>Herramientas para Ingeniería</h2>
+        <h2 class="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl dark:text-white"><span class="animated-gradient text-5xl">Achronyme</span><br>Herramientas para Ingeniería</h2>
         <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">Una suite de herramientas web para el análisis y visualización de conceptos clave del Procesamiento Digital de Señales (PDS), incluyendo la Transformada de Fourier, Series de Fourier, Convolución y más.</p>
     </div>
     <svg aria-hidden="true" class="absolute inset-0 -z-10 size-full mask-[radial-gradient(100%_100%_at_top_right,white,transparent)] stroke-gray-200 dark:stroke-white/10">
